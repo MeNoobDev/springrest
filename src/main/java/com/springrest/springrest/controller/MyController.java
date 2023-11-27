3fa6d4d322a507c94f9c2a939610eda805a49322
MeNoobDev <vedant3532@gmail.com>
2023-11-27 19:13:18 +0530

    added index.html

--- src/main/java/com/springrest/springrest/controller/MyController.java
+++ src/main/java/com/springrest/springrest/controller/MyController.java
@@ -5,54 +5,56 @@ import com.springrest.springrest.services.CourseService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.Optional;
 
-@RestController
+@Controller
 public class MyController {
 
     @Autowired
     private CourseService courseService;
 
-
-    @GetMapping("/home")
-    public String home(){
-        return "This is a Springboot Restapi project. Checkout github for links";
+    // Home endpoint
+    @GetMapping({"/", "/home"})
+    public String home() {
+        return "index"; // Assuming your HTML file is named "index.html"
     }
 
-    // get the courses
+    // Get all courses
     @GetMapping("/courses")
     public List<Course> getCourses(){
         return courseService.getCourses();
     }
 
+    // Get a specific course by ID
     @GetMapping("/courses/{courseId}")
     public Optional<Course> getCourse(@PathVariable String courseId){
         return courseService.getCourse(Long.parseLong(courseId));
     }
 
+    // Add a new course
     @PostMapping("/courses")
     public Course addCourse(@RequestBody Course course){
         return courseService.addCourse(course);
     }
 
-    // update the course using PUT request
+    // Update a course using PUT request
     @PutMapping("/courses")
     public Course updateCourse(@RequestBody Course course){
         return courseService.updateCourse(course);
     }
 
-    // delete the course
+    // Delete a course by ID
     @DeleteMapping("/courses/{courseId}")
     public ResponseEntity<HttpStatus> deleteCourse(@PathVariable String courseId){
         try{
             courseService.deleteCourse(Long.parseLong(courseId));
             return new ResponseEntity<>(HttpStatus.OK);
-        }catch (Exception e) {
+        } catch (Exception e) {
             return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
         }
-
     }
 }
